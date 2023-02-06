21780a87600da3c693ea0259957f256aacac53c3
uyibis <uyibis@gmail.com>
2023-02-06 14:25:57 -0800

    update

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -31,6 +31,7 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
+
     def area(self):
         """
         Calculates the area of a Rectangle instance
