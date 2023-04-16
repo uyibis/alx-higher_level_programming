9ac6d022a6b59015223e638dbbcb78daf6cb7d2e
uyibis <uyibis@gmail.com>
2023-04-16 07:55:18 -0700

    selet update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa
-the script takes 3 arguments
-"""
-if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset='utf8')
-    cur = conn.cursor()
+"""Script that lists all states from database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+def get_states():
+    """Takes arguments argv to list from database
+
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-        
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    get_states()
+    
